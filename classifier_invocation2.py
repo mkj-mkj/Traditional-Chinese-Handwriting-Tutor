811f6881c671f7a01c64ad0d9b3205884ee010db
Rrryu123 <134855498+Rrryu123@users.noreply.github.com>
2024-05-29 16:54:49 +0800

    前端調用模型測試

--- classifier_invocation2.py
+++ classifier_invocation2.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, jsonify, make_response
 import numpy as np
 from tensorflow.keras.models import load_model
 from PIL import Image
 import io
 import os
+from flask import Flask, request, jsonify, make_response
 import json
-
+from flask_cors import CORS
 
 # 使用正確的檔案路徑
 model_path = r'C:\Users\user\Desktop\專題\畢業專題\分類模型\cnn_hanzi_classifier.h5'  # 替換路徑
@@ -24,28 +24,35 @@ label_classes = np.load(labels_path, allow_pickle=True)
 
 # 定義預處理函數
 def preprocess_image(image):
-    img = Image.open(io.BytesIO(image)).convert('RGB').resize((200, 200))
+    img = Image.open(io.BytesIO(image)).convert('RGB').resize((200, 200))  
     img = np.array(img).reshape(1, 200, 200, 3) / 255.0  # 預處理圖片
     return img
 
 app = Flask(__name__)
+CORS(app, resources={r"/predict": {"origins": "http://localhost:8000"}})  # 允許從http://localhost:8000請求
+                                                                          #cd C:\Users\user\Desktop\專題\畢業專題\分類模型
+                                                                          #python -m http.server 8000
+                                                                
 
 @app.route('/predict', methods=['POST'])
-def predict_api(img):
-    if img is None:
-        if 'image' not in request.files:
-            return jsonify({'error': 'No image provided'}), 400
-        
+def predict():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image provided'}), 400
+    
+    try:
         image = request.files['image'].read()
         img = preprocess_image(image)
-
-    prediction = model.predict(img)
-    predicted_class = label_classes[np.argmax(prediction)]
-    
-    # 使用 json.dumps 並設置 ensure_ascii=False 回傳中文
-    response = make_response(json.dumps({'Predicted Character': predicted_class}, ensure_ascii=False))
-    response.headers['Content-Type'] = 'application/json'
-    return response
+        
+        # 進行預測
+        prediction = model.predict(img)
+        predicted_class = label_classes[np.argmax(prediction)]
+        
+        # 使用 json.dumps 並設置 ensure_ascii=False 回傳中文
+        response = make_response(json.dumps({'Predicted Character': predicted_class}, ensure_ascii=False))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
