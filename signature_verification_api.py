c6bdcb2cbcab045eecadb9ac8c071182e126e7f8
M_K_J <kevin911012@gmail.com>
2024-07-14 15:46:27 +0800

    修復前端顯示undefined問題、改成使用signature_verification_prediction來調用模型

--- signature_verification_api.py
+++ signature_verification_api.py
@@ -8,45 +8,32 @@ from flask import Flask, request, jsonify, make_response
 import json
 from flask_cors import CORS
 import pickle
+import joblib
+from signature_verification_prediction import signature_verification
+
+#把numpy class轉換成 Json serializable object
+class JsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (np.integer, np.floating, np.bool_)):
+            return obj.item()
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(JsonEncoder, self).default(obj)
 
 # 使用正確的檔案路徑
-model_path = r'C:\xampp\htdocs\signature_verification\signature_verification.pkl' # 替換路徑
+model_path = r'.\signature_verification.pkl' # 替換路徑
 
 # 確認檔案是否存在
 if not os.path.exists(model_path):
     raise FileNotFoundError(f"Model file not found: {model_path}")
-
-# 載入模型和標籤
-with open(model_path, 'rb') as f:
-    model = pickle.load(f)
-
-# 定義預處理函數
-def preprocess_image(image_bytes):
-    # 將二進位資料轉換為NumPy陣列
-    image_array = np.frombuffer(image_bytes, np.uint8)
-    # 解碼影像
-    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-    
-    # 灰階轉換
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # 二值化
-    _, binary = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
-    # 膨脹
-    kernel = np.ones((2, 2), np.uint8)
-    dilated = cv2.dilate(binary, kernel, iterations=1)
-    # 調整大小
-    resized = cv2.resize(dilated, (256, 256))
-    
-    return resized
-
 
 app = Flask(__name__)
 CORS(app)
 CORS(app, resources={r"/predict": {"origins": "http://localhost:8000"}})  # 允許從http://localhost:8000請求
                                                                           #cd C:\Users\user\Desktop\專題\畢業專題\分類模型
                                                                           #python -m http.server 8000
-                                                                
-
+                                                               
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'image' not in request.files:
@@ -54,14 +41,12 @@ def predict():
     
     try:
         image = request.files['image'].read()
-        img = preprocess_image(image)
-        
         # 進行預測
-        prediction = model.predict(img)
-        #predicted_class = label_classes[np.argmax(prediction)]
-        
+        prediction = signature_verification(image)
+        # print(prediction)
+        # print(type(prediction))
         # 使用 json.dumps 並設置 ensure_ascii=False 回傳中文
-        response = make_response(json.dumps({'Predicted Character': predicted_class}, ensure_ascii=False))
+        response = make_response(json.dumps({'Predicted Character': prediction}, ensure_ascii=False, cls=JsonEncoder))
         response.headers['Content-Type'] = 'application/json'
         return response
     except Exception as e:
